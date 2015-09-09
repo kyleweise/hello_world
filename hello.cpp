e8cdc63731a93e3927da3ad24cb3e779b5bb3ace
kyleweise <weisek@ELABSB138-22.easternct.edu>
2015-09-09 14:39:50 -0400

    Added text
    "Hello User, my name is Kyle!"

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,12 @@ using namespace std;
 int main () {
   
   cout << "Hello Class!\n\n";
+cout << "What is your name?";
+String name = "";
+cin >> name;
+cout << "Hello " + name + ", my name is Kyle."
+
+
 
   return 0;
 }
